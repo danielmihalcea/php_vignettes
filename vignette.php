e2ac353a15d1a6a36883c41dbea60888b19afa2b
Daniel Mihalcea <69532690+danielmihalcea@users.noreply.github.com>
2021-04-21 14:01:49 +0200

    Add files via upload

--- vignette.php
+++ vignette.php
@@ -1,9 +1,9 @@
-<?
+<?php
 /********************************************************************************/
 /* générateur automatique de vignettes en ligne                                 */
 /* gère les formats JPEG, PNG et GIF, préserve le cannal alpha des PNG          */
 /*                                                                              */
-/* copyright Daniel MIHALCEA (c) 2011-2020 http://mihalcea.fr/                  */
+/* copyright Daniel MIHALCEA (c) 2011-2021 http://mihalcea.fr/                  */
 /*                                                                              */
 /* @param   {File}   f: fichier pointant vers l'image source ou URL, peut être  */
 /*                   une URL si allow_url_fopen est activé dans php.ini         */
@@ -14,13 +14,14 @@
 /********************************************************************************/
 
 $f = $_GET['f'] ?? ''; // on récupère le nom du fichier s'il existe
-$h = (int) ( $_GET['h'] ?? '128' ); // on récupère la hauteur, 128 pixels si non défini
-$r = (bool) ( $_GET['r'] ?? '0' ); // doit-on générer une vignette carrée ? Par défaut non
+$h = (int) ( $_GET['h'] ?? 128 ); // on récupère la hauteur, 128 pixels si non défini
+$r = (bool) ( $_GET['r'] ?? false ); // doit-on générer une vignette carrée ? Par défaut non
 
 if ($f{0} == '/') $f = $_SERVER['DOCUMENT_ROOT'].$f; // si le chemin est lié à la racine, on prends la racine du serveur HTTP
 if (!is_numeric ($h) || $h<1) $h = 128; // si la dimension est incorrecte/invalide on prens la dimension par défaut
+$ext = strtolower(substr($f, strrpos($f, '.') + 1)); // on récupère l'extension du fichier
 
-function error($txt) { // génère le message d'erreur en temps qu'image pour pouvoir le voir
+function error(string $txt): void { // génère le message d'erreur en temps qu'image pour pouvoir le voir
     global $h, $im1;
     $im1 = ImageCreateTrueColor($h, $h);
     $blanc = imagecolorallocate($im1, 255, 255, 255);
@@ -52,7 +53,6 @@ if (!is_file($f) && strtolower(substr($f, 0, 7)!='http://') && strtolower(substr
     error('fichier introuvable');
 }
 
-$ext = strtolower(substr($f, strrpos($f, '.') + 1)); // on récupère l'extenssion du fichier
 switch ($ext) { // on envoie l'entête correspondant et on récupère l'image
     case 'jpg' :
     case 'jpeg' : 
@@ -106,4 +106,3 @@ imagesavealpha($im1, true); // permet de conserver le canal alpha
 $r = imagecopyresampled ($im1, $im0, $x, $y, 0, 0, $l1, $h1, $l0, $h0); // redimensionne l'image 
 imagedestroy($im0); // libère la mémoire de suite avant de générer l'image au format choisi
 finalise();
-?>
